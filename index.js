db55d070b3e8f1ec9d4e30b26a418375819c3191
simpart <simpart>
2019-03-08 08:45:29 +0000

    mod hover

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ mf.event.Hover = class extends mofron.Event {
         try {
             super();
             this.name('Hover');
+            this.m_hvrbuf = [];
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -28,17 +29,67 @@ mf.event.Hover = class extends mofron.Event {
                 try {
                     let hvr = p2[0];
                     let flg = p2[1];
-                    hvr.execHandler(flg);
+                    //let lst_tm = hvr.lastKickTime();
+                    //let tm = new Date().getTime();
+                    //hvr.lastKickTime(tm);
+                    //hvr.execHandler(flg);
+                    
+                    if (0 === hvr.hoverBuff().length) {
+                        let exe_hdr = () => {
+                            try {
+                                let buf = hvr.hoverBuff();
+                                hvr.execHandler(buf[buf.length-1]);
+                                hvr.resetBuff();
+                            } catch (e) {
+                                console.error(e.stack);
+                                throw e;
+                            }
+                        };
+                        setTimeout(exe_hdr, 50);
+                    }
+                    hvr.hoverBuff(flg);
                 } catch (e) {
                     console.error(e.stack);
                     throw e;
                 }
             }
             this.component().event([
-                new MsOver(new mf.Param(hdl,[this,true])),
-                new MsOut(new mf.Param(hdl,[this,false]))
+                new MsOver([hdl,[this,true]]), new MsOut([hdl,[this,false]])
             ]);
         } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    hoverBuff (prm) {
+        try {
+            if (undefined === prm) {
+                /* getter */
+                return this.m_hvrbuf;
+            }
+            /* setter */
+            if ('boolean' !== typeof prm) {
+                throw new Error('invalid parameter');
+            }
+            this.m_hvrbuf.push(prm);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    resetBuff () {
+        try {
+            this.m_hvrbuf = [];
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    lastKickTime (prm) {
+        try { return this.member('lastKickTime', 'number', prm, 0); } catch (e) {
             console.error(e.stack);
             throw e;
         }
